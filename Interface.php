47a8404836cbba05a33004fc7def2ceb2d89638a
jbecares <julibecares@hotmail.com.ar>
2016-09-29 12:41:24 -0300

    Delete Interface.php

--- Interface.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface Tarjeta{
-	public function pagar($transporte, $hora, $fecha);
-	public function recargar($monto);
-	public function saldo();
-	public function viajesRealizados();
-}
-?>
